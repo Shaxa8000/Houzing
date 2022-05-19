4ee4a7fc51a2b7474315a1915dac563ace8b88f3
Shaxa8000 <shaxboz.yaxyoyev@bk.ru>
2022-05-19 18:22:45 +0500

    Navbar completed

---
+++ src/root/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { navbar } from '../utils/navbar';
+import { Container } from './style'
+import Navbar from '../components/Navbar';
+
+
+export const Root = () => {
+  return (
+      <Container>
+          <Navbar/>
+      </Container>
+  )
+}
+
