cde296258da3b5ae282231740dffdaf15e14414a
Your Name <shaxboz.yaxyoyev@bk.ru>
2023-02-17 11:04:55 +0500

    initialized

--- src/root/index.jsx
+++ src/root/index.jsx
@@ -1,36 +1,9 @@
-import React from 'react';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import { navbar } from '../utils/navbar';
-import Footer from '../components/Footer';
-// import { Container } from "./style";
+import React from 'react'
 
-export const Root = () => {
+const Root = () => {
   return (
-    <>
-      <Routes>
-        <Route
-          path='/profile'
-          element={<Navigate to={'/profile/properties'} />}
-        />
-        <Route element={<Navbar />}>
-          {navbar.map(({ path, Element, id, param }) => {
-            return param && <Route key={id} path={path} element={Element} />;
-          })}
-        </Route>
-        <Route element={<Navbar />}>
-          {navbar.map(({ path, Element, id, hidden }) => {
-            return (
-              /*!hidden &&*/ <Route key={id} path={path} element={Element} />
-            );
-          })}
-          <Route path='/' element={<Navigate to={'/home'} />} />
-        </Route>
-        <Route path='*' element={<div>page not found</div>} />
-      </Routes>
-      <Footer/>
-    </>
-  );
-};
+    <div>Root</div>
+  )
+}
 
-export default Root;
+export default Root
